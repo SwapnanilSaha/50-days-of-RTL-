eec022f58e90db7673a4b6df2bd4bf286ac11f75
SwapnanilSaha <78206702+SwapnanilSaha@users.noreply.github.com>
2023-07-18 02:30:45 +0530

    Add files via upload

---
+++ 4X1 Mux using 2 to 4 decoder and tri-state buffer/decoder.v
@@ -0,0 +1,9 @@
+module decoder(en,d,y);
+input [1:0]d;
+input en;
+output [3:0]y;
+assign y[0]=(en&~d[0]&~d[1]);
+assign y[1]=(en&~d[1]&d[0]);
+assign y[2]=(en&d[1]&~d[0]);
+assign y[3]=(en&d[1]&d[0]);
+endmodule
