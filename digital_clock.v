5c0fa3e2d3717a599263414bb6d8adb338b0fb83
SwapnanilSaha <78206702+SwapnanilSaha@users.noreply.github.com>
2024-04-21 12:38:50 +0530

    Add files via upload

--- digital_clock.v
+++ digital_clock.v
@@ -1,11 +1,11 @@
-module digital_clock(clcock,reset,load,load_ms_hr,load_ls_hr,load_ms_min,load_ls_min,time_ms_hr,time_ls_hr,time_ms_min,time_ls_min);
-input  clcock,reset,load;
-input [2:0]load_ls_hr;
+module digital_clock(clock,reset,load,load_ms_hr,load_ls_hr,load_ms_min,load_ls_min,time_ms_hr,time_ls_hr,time_ms_min,time_ls_min);
+input  clock,reset,load;
+input [3:0]load_ls_hr;
 input [3:0]load_ls_min;
 input [1:0]load_ms_hr;
 input [2:0]load_ms_min;
-output reg[1:0]time_ms_hr,
-output reg [2:0]time_ls_hr;
+output reg[1:0]time_ms_hr;
+output reg [3:0]time_ls_hr;
 output reg [2:0]time_ms_min;
 output reg[3:0]time_ls_min;
 always@(posedge clock or negedge reset)
@@ -23,25 +23,29 @@ if(load)
 begin
 time_ls_hr<=load_ls_hr;
 time_ms_hr<=load_ms_hr;
-time_ls_min<=load_ms_min;
+time_ls_min<=load_ls_min;
 time_ms_min<=load_ms_min;
 end
 else
 begin
 time_ls_min<=time_ls_min+1;
-if(time_ls_min==10)
+if(time_ls_min==9)
 begin
 time_ls_min<=0;
 time_ms_min<=time_ms_min+1;
-if(time_ms_min==6)
+if(time_ms_min==5)
 begin
 time_ms_min<=0;
 time_ls_hr<=time_ls_hr+1;
-if(time_ls_hr==4)
+if(time_ls_hr==9)
 begin
 time_ls_hr<=0;
 time_ms_hr<=time_ms_hr+1;
-if(time_ms_hr==3)
+end
+else if(time_ls_hr==3 && time_ms_hr==2)
+begin
+time_ls_hr<=0;
+if(time_ms_hr==2)
 begin
 time_ms_hr<=0;
 end
